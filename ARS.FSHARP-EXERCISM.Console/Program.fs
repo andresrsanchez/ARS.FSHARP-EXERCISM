9c9cff2493fc853954b1578208207ec0e44a564f
Andrés Rodriguez <andres.rodriguez@eit.es>
2018-04-16 18:17:05 +0200

    developing bob exercise, a lot of regex involved

--- ARS.FSHARP-EXERCISM.Console/Program.fs
+++ ARS.FSHARP-EXERCISM.Console/Program.fs
@@ -1,12 +1,25 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Text.RegularExpressions
 
 let exercism_Year = 
     printfn "Give me a year: "
     let year = System.Console.ReadLine() |> int
     printf "Result : %b" (year % 4 = 0 && year % 100 <> 0 && year % 400 = 0) //leap condition
     Console.ReadLine() |> ignore
+
+let exercism_Bob = 
+    while true do
+        printfn "Tell me something: "
+        let something = System.Console.ReadLine()
+        let (|FirstRegexGroup|_|) pattern input =
+            let m = Regex.Match(input,pattern) 
+            if (m.Success) then Some m.Groups.[1].Value else None 
+        match something with
+        | FirstRegexGroup "^¿?(.*)\?" something -> printf "Sure."
+        | FirstRegexGroup "^[A-Z]*$" something -> printf "Whoa, chill out!"
+        | _ -> printfn "WTF u want to say"
 
 [<EntryPoint>]
 let main argv =
