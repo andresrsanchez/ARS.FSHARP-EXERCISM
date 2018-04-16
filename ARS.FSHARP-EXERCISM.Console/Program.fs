b1c26349415a1c7afb7b7d181c4695af76d4d933
Andrés Rodriguez <andres.rodriguez@eit.es>
2018-04-16 13:33:08 +0200

    created year function, without review leap condition (not interest on this logic)

--- ARS.FSHARP-EXERCISM.Console/Program.fs
+++ ARS.FSHARP-EXERCISM.Console/Program.fs
@@ -2,7 +2,14 @@
 
 open System
 
+let exercism_Year = 
+    printfn "Give me a year: "
+    let year = System.Console.ReadLine() |> int
+    printf "Result : %b" (year % 4 = 0 && year % 100 <> 0 && year % 400 = 0) //leap condition
+    Console.ReadLine() |> ignore
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    exercism_Year |> ignore
     0 // return an integer exit code
+
